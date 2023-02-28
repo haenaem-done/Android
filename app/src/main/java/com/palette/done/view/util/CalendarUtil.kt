e2f8172dc2d5a2abe4d8fccdc692b67584d454a2
koownij <koownij@kakao.com>
2023-02-28 15:56:17 +0900

    feat: 간편하게 가장 근접한 hour, minute에 해당하는 timeInMillis를 가져오는 CalendarUtil 추가

---
+++ app/src/main/java/com/palette/done/view/util/CalendarUtil.kt
@@ -0,0 +1,16 @@
+package com.palette.done.view.util
+
+import java.util.Calendar
+
+object CalendarUtil {
+    fun getNextTimeInMillisFromNow(hour: Int, minute: Int): Long {
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.set(Calendar.HOUR_OF_DAY, hour)
+        calendar.set(Calendar.MINUTE, minute)
+        calendar.set(Calendar.SECOND, 0)
+        if(System.currentTimeMillis() > calendar.timeInMillis)
+            calendar.add(Calendar.DATE, 1)
+
+        return calendar.timeInMillis
+    }
+}
